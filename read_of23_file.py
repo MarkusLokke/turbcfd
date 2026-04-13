d007705e3b440c5c7a5948d1bd3cb628915e6125
BigBaush <115996763+BigBaush@users.noreply.github.com>
2026-04-13 22:01:13 +0200

    - Laget ny funksjon for lesing av openFOAM filer inni read_of23_file.py, - Opdatert funksjonsbeskrivelse i read_yLineUdata.py

---
+++ read_of23_file.py
@@ -0,0 +1,202 @@
+####################### Imports #######################
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+####################### Read nut file #######################
+
+def read_nut_file(path, skip_lines = 23, read_length = 1000):
+    """
+    Reads a _nut_ datafile and splits creates a numpy array from file. 
+    The function requires that the elements are separated using space and not comma. 
+
+    Args:
+        path (_string_): Path is the relative path of the file you want to read and convert to a np.array
+        skip_lines (_int_): is the amount of lines to skip before adding to your dataset datset. 23 Lines by default
+        read_Length (_int_): is the length of the dataset you want to read. 1000 lines by default
+    """
+    
+    #### Creating Output and parameters ####
+    nu_t = np.zeros( [read_length] )
+    counter = 0
+
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        #### find data size ####        
+        for line in lines:
+            #### count lines and check if it should skip ####
+            counter +=1 
+            if counter <= skip_lines:
+                continue
+            elif counter > read_length + skip_lines:
+                break
+
+            #### Processing line into list ####
+            line = line.strip()
+            line = line.replace(")", "").replace('(', '')
+            lines_list = line.split() 
+            
+            #### Adding to output ####
+            nu_t [counter - skip_lines - 1] = lines_list[0]
+    return nu_t
+
+#### Usage Example ####
+
+# from read_of23_file import read_nut_file
+# path3 = "DNS1000/kwsst/nut"
+
+# nut = read_nut_file(path3)
+# # nut = read_nut_file(path3, 23, 1000) #Use when the file does not follow standard layout, I.E not: 23, 1000
+
+# print(nut)
+# print(len(nut))
+# print(type(nut))
+
+
+
+####################### Read wallShearStress file #######################
+
+def read_wallShearStress_file(path):
+    """
+    Reads a _wallShearStress_ datafile and returns the last element. 
+    The function requires that the elements are separated using space and not comma. 
+
+    Args:
+        path (_string_): Path is the relative path of the file you want to read
+        u_tau (_float_): Output value for u_tau
+    """
+    #### Creating Output and parameters ####
+    with open(path, 'r') as file:
+        last_line = file.readlines()[-1]
+        u_tau = np.sqrt(abs(float(last_line.split()[2].strip('()'))))
+    return u_tau
+
+#### Usage Example ####: 
+
+# from read_of23_file import read_wallShearStress_file
+# path4 = "DNS1000/kwsst/wallShearStress.dat"
+
+# u_tau_1 = read_wallShearStress_file(path4)
+
+# print(u_tau_1)
+# print(type(u_tau_1))
+
+
+
+####################### Read U file #######################
+
+def read_U_file(path, skip_lines = 23, read_length = 1000):
+    """
+    Reads a _U_ datafile and splits creates a numpy array from file. 
+    The function requires that the elements are separated using space and not comma. 
+
+    Args:
+        path (_string_): Path is the relative path of the file you want to read and convert to a np.array
+        skip_lines (_int_): is the amount of lines to skip before adding to your dataset datset. 23 Lines by default
+        read_Length (_int_): is the length of the dataset you want to read. 1000 lines by default
+    """
+    
+    #### Creating Output ####
+    # file_data = np.zeros( [read_length, cols])
+    U1 = np.zeros( [read_length])
+    U2 = np.zeros( [read_length])
+    U3 = np.zeros( [read_length])
+
+    with open(path, 'r') as file:
+        lines = file.readlines()
+        #### find data size ####
+        rows = len(lines)
+        counter = 0
+        
+        for line in lines:
+            #### count lines and check if it should skip ####
+            counter +=1 
+            if counter <= skip_lines:
+                continue
+            elif counter > read_length + skip_lines:
+                break
+
+            #### Processing line into list ####
+            line = line.strip()
+            line = line.replace(")", "").replace('(', '')
+            lines_list = line.split() 
+            
+            #### Adding to output ####
+            U1 [counter - skip_lines - 1] = lines_list[0]
+            U2 [counter - skip_lines - 1] = lines_list[1]
+            U3 [counter - skip_lines - 1] = lines_list[2]
+            # outlist = []
+            # for idx in range(len(lines_list)):
+            #     try:
+            #         outlist.append( float(lines_list[idx]) )
+            #     except Exception:
+            #         continue
+            # file_data[ counter - skip_lines - 1 ] = outlist
+    return U1, U2, U3
+    # return file_data
+
+#### Usage Example ####: 
+# from read_of23_file import read_U_file
+# path2 = "DNS1000/kwsst/U"
+
+# u1, u2, u3 = read_U_file(path2)
+# # u1, u2, u3 = read_U_file(path2, 23, 1000) #Use when the file does not follow standard layout, I.E not: 23, 1000
+
+# print(u1)
+# print(len(u1))
+# print(type(u1))
+
+
+
+####################### Read CFD SIM RAW DATA #######################
+
+def read_cfd_sim(folder_path):
+    """
+    U1, U2, U3, nut, u_tau = read_cfd_sim(folder_path)
+    
+    Reads _U_, _nut_ and _wallShearStress_ datafiles and returns all relevant variables. 
+    The function requires that the files use the standard names to properly read the data. 
+
+    Args:
+        folder_path (_string_): relative path to the folder containing the given simulation
+        
+        U1 (_np.Array_): Output np.array for U1
+        U2 (_np.Array_): Output np.array for U2
+        U3 (_np.Array_): Output np.array for U3
+        nut (_np.Array_): Output np.array for nut
+        u_tau (_np.float_): Output value for u_tau
+    """
+    nut_path = folder_path + "/nut"
+    nut = read_nut_file(nut_path)
+
+    tau_path = folder_path + "/wallShearStress.dat"
+    u_tau = read_wallShearStress_file(tau_path)
+
+    U_path = folder_path + "/U"
+    U1, U2, U3 = read_U_file(U_path)
+    
+    return U1, U2, U3, nut, u_tau
+    
+#### Usage Example ####
+# from read_of23_file import read_cfd_sim
+
+# folder_path = "DNS1000/kwsst"
+# U1, U2, U3, nut, u_tau = read_cfd_sim(folder_path)
+
+# print(len(U1))
+# print(type(U1))
+
+# print(len(U2))
+# print(type(U2))
+
+# print(len(U3))
+# print(type(U3))
+
+# print(len(nut))
+# print(type(nut))
+
+# print(u_tau)
+# print(type(u_tau))
--- read_yLineUdata.py
+++ read_yLineUdata.py
@@ -1,23 +1,39 @@
+#### Imports ####
+
 import sys
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+#### Function ####
+
 def read_yLineUdata(path):
     """
-    _summary_
-        The function reads yLine_U_non_uniform.xy file and outputs y, U1, U2, from a given "path"
-    _Arguments_
-        Inputs:
-            path    : input file path  (str)
-        Output: 
-            y       : y-output value   (npArray)
-            U1      : U1-output value  (npArray)
-            U2      : U2-output value  (npArray)
+    Summary:
+    The function reads yLine_U_non_uniform.xy file and outputs y, U1, U2, from a given "path"
+        
+    Inputs:
+    path    : input file path  (str)
+        
+    Output:
+    y       : y-output value   (np.Array)
+    U1      : U1-output value  (np.Array)
+    U2      : U2-output value  (np.Array)
     """
     # Reading yLine_U_non_uniform.xy
     data = np.loadtxt(path, delimiter='\t')
-    y = data[:, 0]
-    U1 = data[:, 1]
-    U2 = data[:, 2]
+    y = np.array(data[:, 0])
+    U1 = np.array(data[:, 1])
+    U2 = np.array(data[:, 2])
     return y, U1, U2
+
+#### Usage Example ####
+
+# from read_yLineUdata import read_yLineUdata
+# path1 = "DNS1000/kwsst/yLine_U_non_uniform.xy"
+
+# y, u1, u2 = read_yLineUdata(path1)
+
+# print(u2)
+# print(len(u2))
+# print(type(u2))
