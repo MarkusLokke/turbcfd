ac7cd0631b90eb55d68a3108a44eb603864ba4d7
BigBaush <115996763+BigBaush@users.noreply.github.com>
2026-04-13 15:31:03 +0200

    - Added template for readdata

---
+++ read_yLineUdata.py
@@ -0,0 +1,18 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+def read_yLineUdata(path):
+    """
+    _summary_
+    Inputs:
+        path    : input file path  (str)
+    Output: 
+        y       : y-output value   (npArray)
+        U1      : U1-output value  (npArray)
+        U2      : U2-output value  (npArray)
+    """
+    
+    
+    
