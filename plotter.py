ce783adbeda241b6087b03025ca9fb339521e11a
Brage Iversen <156439400+BraIve@users.noreply.github.com>
2026-04-16 17:08:02 +0200

    first func works

--- plotter.py
+++ plotter.py
@@ -4,6 +4,9 @@ import matplotlib.pyplot as plt
 from read_of23_file import read_cfd_sim
 from read_yLineUdata import read_yLineUdata
 
+nus = [5e-5, 8e-6, 1.515e-5, 1.515e-5]
+hs  = [1.00, 1.00, 0.025000, 0.025000]
+
 def u1u1(TKE):
     return 2/3 * TKE
 
@@ -14,38 +17,56 @@ def u1u2(U, x_2, nu_tau):
      return - nu_tau * np.gradient(U, x_2)
 
 
-def plotter_same_mesh(modelpaths, models, case):
+def plotter_same_mesh(modelpaths, models, refpath, case, H, nu):
      '''
      This function does some stuff
      '''
      fig, axs = plt.subplots(2,2)
-     for i in range(len(modelpaths)):
+
+     i=0
+     while(i<len(modelpaths)):
           U1, U2, U3, nut, k, u_tau = read_cfd_sim(modelpaths[i])
-          y, a, b = read_yLineUdata(modelpaths[i] + "/yLine_U_non_uniform.xy")
-          print(np.shape(y), np.shape(nut))
-          y_plus = y * u_tau/nut
+          y = np.linspace(0,H,1000)
+          print(np.shape(nut), np.shape(u_tau))
 
-          axs[0,0].plot(U1/u_tau, y_plus, label=models[i])
+          y_plus = y * u_tau/nu
 
-          axs[0,1].plot(u1u1(k)/u_tau**2, y_plus, label=models[i])
+          axs[0,0].semilogx(y_plus, U1/u_tau, label=models[i])
 
-          axs[1,0].plot(u2u2(k)/u_tau**2, y_plus, label=models[i])
+          axs[0,1].semilogx(y_plus, u1u1(k)/u_tau**2,  label=models[i])
 
-          axs[1,1].plot(u1u2(U1, y, nut)/u_tau**2, y_plus, label=models[i])
+          axs[1,0].semilogx(y_plus, u2u2(k, U2, y, nut)/u_tau**2,  label=models[i])
+
+          axs[1,1].semilogx(y_plus, -u1u2(U1, y, nut)/u_tau**2,  label=models[i])
+     
+          i+=1
+
+     #printing reference data
 
 
      #labels
-     axs[0,0].setylabel(r'$U_1^\plus$')
-     axs[0,0].setxlabel(r'$x_2^\plus$')
+     axs[0,0].set_ylabel(r'$U_1^\plus$')
+     axs[0,0].set_xlabel(r'$x_2^\plus$')
+     axs[0,0].set_xlim(10**(-2), 10**(4))
+     axs[0,0].set_ylim(bottom=0)
      axs[0,0].legend()
-     axs[0,1].setylabel(r"$\overline{u_1' u_1'}^\plus$")
-     axs[0,1].setxlabel(r'$x_2^\plus$')
+
+     axs[0,1].set_ylabel(r"$\overline{u_1' u_1'}^\plus$")
+     axs[0,1].set_xlabel(r'$x_2^\plus$')
+     axs[0,1].set_xlim(10**(-2), 10**(4))
+     axs[0,1].set_ylim(bottom=0)
      axs[0,1].legend()
-     axs[1,0].setylabel(r"$\overline{u_2' u_2'}^\plus$")
-     axs[1,0].setxlabel(r'$x_2^\plus$')
+
+     axs[1,0].set_ylabel(r"$\overline{u_2' u_2'}^\plus$")
+     axs[1,0].set_xlabel(r'$x_2^\plus$')
+     axs[1,0].set_xlim(10**(-2), 10**(4))
+     axs[1,0].set_ylim(bottom=0)
      axs[1,0].legend()
-     axs[1,1].setylabel(r"$\overline{u_1' u_2'}^\plus$")
-     axs[1,1].setxlabel(r'$x_2^\plus$')
+
+     axs[1,1].set_ylabel(r"$- \overline{u_1' u_2'}^\plus$")
+     axs[1,1].set_xlabel(r'$x_2^\plus$')
+     axs[1,1].set_xlim(10**(-2), 10**(4))
+     axs[1,1].set_ylim(bottom=0)
      axs[1,1].legend()
 
      #room for aesthetic changes here, probably needed
@@ -56,9 +77,14 @@ def plotter_same_mesh(modelpaths, models, case):
 
 #testing function
 
-paths_1 = ["DNS1000/kwsst"]
-models_1 = [r"k-\omega SST"]
-plotter_same_mesh(paths_1, models_1, r"$Re = 1000$")
+paths_1 = ["DNS1000/kep", "DNS1000/kepRNG", "DNS1000/kw", "DNS1000/kwsst"] #, "DNS1000/spalart-allmaras"
+models_1 = [r"$k$-$\epsilon$", 
+            r"$k$-$\epsilon$ RNG",
+            r"$k$-$\omega$", 
+            r"$k$-$\omega$ SST",
+            ]
+ref_path_1 = "DNS_1000_DNS/1000_dataset.mat"
+plotter_same_mesh(paths_1, models_1, ref_path_1, r"$Re = 1000$", 2, nus[1])
 
 
 def plotter_same_model(grid_model_paths, grids, models, case):
