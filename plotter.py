2c807e3ac588f4870d44939771735332d3550603
Brage Iversen <156439400+BraIve@users.noreply.github.com>
2026-04-16 13:02:53 +0200

    created plotter_same_mesh

--- plotter.py
+++ plotter.py
@@ -1,19 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-U_tau = 1
+from read_of23_file import read_cfd_sim
 
-def u1_std(k):
-    return 2/3 * k
+def u1u1(TKE):
+    return 2/3 * TKE
 
-def u2_std(k, U2, y, nut):
-     return 2/3 * k - 2 * nut * np.gradient(U2, y)
+def u2u2(TKE, V, x_2, nu_tau):
+     return 2/3 * TKE - 2 * nu_tau * np.gradient(V, x_2)
 
-def u1u2(U1, y, nut):
-     return - nut * np.gradient(U1, y)
+def u1u2(U, x_2, nu_tau):
+     return - nu_tau * np.gradient(U, x_2)
 
 
-def plotter_same_mesh(modelpaths, models):
+def plotter_same_mesh(modelpaths, models, case):
+     '''
+     This function does some stuff
+     '''
+     fig, axs = plt.subplots(2,2)
+     for i in range(len(modelpaths)):
+          U1, U2, U3, nut, u_tau = read_cfd_sim(modelpaths[i])
+          y = 0
+          k = 0
+
+          y_plus = y * u_tau/nut
+
+          axs[0,0].plot(U1/u_tau, y_plus, label=models[i])
+
+          axs[0,1].plot(u1u1(k)/u_tau**2, y_plus, label=models[i])
+
+          axs[1,0].plot(u2u2(k)/u_tau**2, y_plus, label=models[i])
+
+          axs[1,1].plot(u1u2(U1, y, nut)/u_tau**2, y_plus, label=models[i])
+
+
+     #labels
+     axs[0,0].setylabel(r'$U_1^\plus$')
+     axs[0,0].setxlabel(r'$x_2^\plus$')
+     axs[0,0].legend()
+     axs[0,1].setylabel(r"$\overline{u_1' u_1'}^\plus$")
+     axs[0,1].setxlabel(r'$x_2^\plus$')
+     axs[0,1].legend()
+     axs[1,0].setylabel(r"$\overline{u_2' u_2'}^\plus$")
+     axs[1,0].setxlabel(r'$x_2^\plus$')
+     axs[1,0].legend()
+     axs[1,1].setylabel(r"$\overline{u_1' u_2'}^\plus$")
+     axs[1,1].setxlabel(r'$x_2^\plus$')
+     axs[1,1].legend()
+
+     #room for aesthetic changes here
+
+     fig.savefig(case + "_plots.py")
+     fig.show()
      return 0
 
 
